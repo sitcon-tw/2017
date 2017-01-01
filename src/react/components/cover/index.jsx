a8fc710711cf4290d357888039afbd89c2bfee3b
Sunset <s2821d3721@gmail.com>
2017-01-01 15:59:45 +0800

    Update cover page

--- src/react/components/cover/index.jsx
+++ src/react/components/cover/index.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
-import paragraphIntro from './paragraphIntro'
+import paragraphIntro from './paragraphIntro.json'
+import paragraphSlogan from './paragraphSlogan.json'
+import paragraphAboutUs from './paragraphAboutUs.json'
 
 export default class extends Component {
 	render() {
 		return (
 			<div id="cover-page">
+
+				<article className="content-container">
+				</article>
+
 				<article className="content-container">
 					<h2>Internet For Next Generation</h2>
 					{paragraphIntro.map((text, id) => {
 						return <p key={id}>{text}</p>;
 					})}
 				</article>
+
+				<article className="content-container">
+					<section>
+						<h2>Innovation &amp; Implementation</h2>
+						{paragraphSlogan.map((text, id) => {
+							return <p key={id}>{text}</p>;
+						})}
+					</section>
+
+					<section>
+						<h2>About Us</h2>
+						{paragraphAboutUs.map((text, id) => {
+							return <p key={id}>{text}</p>;
+						})}
+					</section>
+
+					<a id="code-of-conduct">行為準則 / Code of Conduct </a>
+				</article>
+
 			</div>
 		)
 	}
