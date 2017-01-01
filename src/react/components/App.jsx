ffb32cec5c664f8ac5b4a94c34383b4790b32f03
Sunset <s2821d3721@gmail.com>
2017-01-01 13:24:10 +0800

    Setup react pages structures

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import Cover from './cover/index.jsx'
+import Schedule from './schedule/index.jsx'
+import Sponsor from './sponsor/index.jsx'
+import Transport from './transport/index.jsx'
+import Navbar from './navbar/index.jsx'
 
 export default class App extends Component {
 	render() {
 		return (
-			<h1>SITCON 2017</h1>
+			<div id="app">
+				<h1>SITCON 2017</h1>
+				<Cover id="cover-page" />
+				<Schedule id="schedule-page" />
+				<Sponsor id="sponsor-page" />
+				<Transport id="transport-page" />
+				<Navbar />
+			</div>
 		)
 	}
 }
---
+++ src/react/components/navbar/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+	render() {
+		return (
+			<nav>
+				<a href="#cover-page">首頁</a>
+				<a href="#schedule-page">議程</a>
+				<a href="#sponsor-page">贊助</a>
+				<a href="#transport-page">交通</a>
+			</nav>
+		)
+	}
+}
