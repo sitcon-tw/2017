30085b65662011609bbb996850f3b9969b6a036c
Sunset <s2821d3721@gmail.com>
2017-01-30 16:38:13 +0800

    [background] Update style

--- src/react/components/background/index.jsx
+++ src/react/components/background/index.jsx
@@ -4,7 +4,8 @@ export default class extends Component {
 	render() {
 		return (
 			<div id="background-house-container">
-				<img src="images/background-house.png" />
+				<img src="images/background.png" />
+				<img src="images/background-house.png" role="house" />
 			</div>
 		)
 	}
