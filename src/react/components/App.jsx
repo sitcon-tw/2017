e4e36f0a5fa1005fe29e98514c4ab36461e16d67
Sunset <s2821d3721@gmail.com>
2017-01-30 23:19:07 +0800

    [viewer] propagate which page is active

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -5,16 +5,34 @@ import Sponsor from './sponsor/index.jsx'
 import Transport from './transport/index.jsx'
 import Navbar from './navbar/index.jsx'
 import Background from './background/index.jsx'
+import * as pageStore from 'stores/pages.js'
 
 class Viewer extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { page: pageStore.state.page }
+	}
+	
+	componentDidMount() {
+		pageStore.register(() => {this.pageChangeHandler()})
+	}
+
+	pageChangeHandler() {
+		this.setState({ page: pageStore.state.page })
+	}
+
 	render() {
+		let isCover = (this.state.page === '#cover-page')
+		let isShedule = (this.state.page === '#schedule-page')
+		let isSponsor = (this.state.page === '#sponsor-page')
+		let isTransport = (this.state.page === '#transport-page')
 		return (
 			<div id="viewer">
 				<div className="content-container">
-					<Cover />
-					<Schedule />
-					<Sponsor />
-					<Transport />
+					<Cover active={isCover}/>
+					<Schedule active={isShedule} />
+					<Sponsor active={isSponsor} />
+					<Transport active={isTransport} />
 				</div>
 				<div className="side-decorator"></div>
 			</div>
