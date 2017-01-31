7403ee24f93fbe225007fd89fe9993ca9d8341d2
Sunset <s2821d3721@gmail.com>
2017-01-31 15:26:28 +0800

    [schedule] Update table

--- src/react/components/schedule/col.jsx
+++ src/react/components/schedule/col.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 export default function(props) {
 	const data = props.data
+	const photo = {
+		backgroundImage: "url(images/stone-sm.png)",
+	}
 	return (
 		<div className="col">
 			<div role="talk-title">{data.title}</div>
 			<div role="talk-speaker">
+				<div role="photo" style={photo}></div>
 				{data.speaker.profile.display_name}
 			</div>
 			<div role="talk-slide">
