9986d642133bc4553f370c40778b23ed1a30ce6b
Sunset <s2821d3721@gmail.com>
2017-01-31 09:01:50 +0800

    [sponsor] Add sponsor list

--- src/react/components/sponsor/index.jsx
+++ src/react/components/sponsor/index.jsx
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
+import * as sponsors from './sponsor.jsx'
+
+function Sponsors(props) {
+	const list = props.list.map((sponsor, key) => {
+		return (
+			<div className="sponsor" key={key}>
+				<img src={"images/logos/"+sponsor.logo} />
+				<p>{sponsor.name}</p>
+			</div>
+		)
+	})
+	return (
+		<div className="sponsor-class-list">
+			{list}
+		</div>
+	)
+}
 
 export default class extends Component {
 	render() {
 		return (
 			<div id="sponsor-page" className={this.props.active ? 'active' : ''}>
-				<h2>Sponsor</h2>
+				<section>
+					<h2>共同主辦</h2>
+					<Sponsors list={sponsors.organizer} />
+				</section>
+				<section>
+					<h2>協辦單位</h2>
+					<Sponsors list={sponsors.coorganizer} />
+				</section>
+				<section>
+					<h2>黃金級贊助</h2>
+					<Sponsors list={sponsors.golden} />
+				</section>
+				<section>
+					<h2>白銀級贊助</h2>
+					<Sponsors list={sponsors.silver} />
+				</section>
+				<section>
+					<h2>青銅級贊助</h2>
+					<Sponsors list={sponsors.bronze} />
+				</section>
+				<section>
+					<h2>特別感謝</h2>
+					<Sponsors list={sponsors.thanks} />
+				</section>
+				<section>
+					<h2>媒體夥伴</h2>
+					<Sponsors list={sponsors.medias} />
+				</section>
 			</div>
 		)
 	}
---
+++ src/react/components/sponsor/sponsor.jsx
@@ -0,0 +1,80 @@
+export const organizer = [
+	{
+		name: "SITCON 學生計算機年會籌備團隊",
+		logo: "sitcon.png",
+	},
+	{
+		name: "財團法人西坑文化基金會",
+		logo: "sitcon.png",
+	},
+]
+
+export const coorganizer = [
+	{
+		name: "西坑研究院資訊科學研究所",
+		logo: "sitcon.png",
+	},
+	{
+		name: "西坑資通訊軟體創新人才推升計畫",
+		logo: "sitcon.png",
+	},
+]
+
+export const golden = [
+	{
+		name: "西坑銀行",
+		logo: "sitcon.png",
+	},
+	{
+		name: "西坑有點軟",
+		logo: "sitcon.png",
+	},
+]
+
+export const silver = [
+	{
+		name: "sitcondo",
+		logo: "sitcon.png",
+	},
+	{
+		name: "sitconsky",
+		logo: "sitcon.png",
+	},
+]
+
+export const bronze = [
+	{
+		name: "五倍吸坑",
+		logo: "sitcon.png",
+	},
+	{
+		name: "西坑科技",
+		logo: "sitcon.png",
+	},
+	{
+		name: "西坑基金會",
+		logo: "sitcon.png",
+	},
+]
+
+export const thanks = [
+	{
+		name: "西坑青年發展署促進青年公共參與補助",
+		logo: "sitcon.png",
+	},
+	{
+		name: "中國西坑大學",
+		logo: "sitcon.png",
+	},
+]
+
+export const medias = [
+	{
+		name: "西坑傳媒",
+		logo: "sitcon.png",
+	},
+	{
+		name: "SitconNews",
+		logo: "sitcon.png",
+	},
+]
