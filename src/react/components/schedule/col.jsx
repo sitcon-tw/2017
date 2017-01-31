71445ae60a08a4991b4e996f296d70048a215aa9
Sunset <s2821d3721@gmail.com>
2017-01-31 21:59:11 +0800

    [schedule] Stop propagation on slide link

--- src/react/components/schedule/col.jsx
+++ src/react/components/schedule/col.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 
-export default function(props) {
-	const data = props.data
-	const photo = {
-		backgroundImage: "url(images/stone-sm.png)",
+export default class extends Component {
+	stopPropagation(e) {
+		console.log('????', e)
+	    e.stopPropagation()
+	    e.nativeEvent.stopImmediatePropagation()
 	}
-	return (
-		<div className="col">
-			<div role="talk-title">{data.title}</div>
-			<div role="talk-speaker">
-				<div role="photo" style={photo}></div>
-				{data.speaker.profile.display_name}
+
+	render() {
+		const data = this.props.data
+		const photo = {
+			backgroundImage: "url(images/stone-sm.png)",
+		}
+		return (
+			<div className="col">
+				<div role="talk-title">{data.title}</div>
+				<div role="talk-speaker">
+					<div role="photo" style={photo}></div>
+					{data.speaker.profile.display_name}
+				</div>
+				<div role="talk-slide">
+					<a href={data.slides} target="blank" onClick={this.stopPropagation}>#簡報連結</a>
+				</div>
 			</div>
-			<div role="talk-slide">
-				<a href={data.slides} target="blank">#簡報連結</a>
-			</div>
-		</div>
-	)
+		)
+	}
 }
