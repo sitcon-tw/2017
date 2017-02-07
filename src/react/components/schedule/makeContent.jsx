71f55b080d8834c34d10b0ffce5fcfbad8b8118c
Sunset <s2821d3721@gmail.com>
2017-02-08 02:48:17 +0800

    [schedule] Split speaker info with \n

--- src/react/components/schedule/makeContent.jsx
+++ src/react/components/schedule/makeContent.jsx
@@ -8,17 +8,22 @@ export default function makeContent(props) {
 	const slide = props.slides
 	const name = props.speaker.name
 	const bio = props.speaker.bio
+
 	return () => {
 		return (
 			<div>
 				<section>
 					<div role="photo" style={photo}></div>
 					<h2>{name}</h2>
-					<p>{bio}</p>
+					{bio.split('\n').map((paragraph, idx) => {
+						return <p key={idx}>{paragraph}</p>
+					})}
 				</section>
 				<section>
 					<h2>{subject}</h2>
-					<p>{summary}</p>
+					<p>{summary.split('\n').map((paragraph, idx) => {
+						return <p key={idx}>{paragraph}</p>
+					})}</p>
 					<p></p>
 				</section>
 			</div>
