700fec681502a0c88b98681a93dc50fd7f2bf4d1
Sunset <s2821d3721@gmail.com>
2017-02-08 17:10:26 +0800

    [sponsor] Remove debug sponsor

--- src/react/components/sponsor/sponsor.jsx
+++ src/react/components/sponsor/sponsor.jsx
@@ -6,112 +6,22 @@ export const organizer = [
 		logo: "sitcon.png",
 		intro: sitconIntro,
 	},
-	{
-		name: "財團法人西坑文化基金會",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const coorganizer = [
-	{
-		name: "西坑研究院資訊科學研究所",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑資通訊軟體創新人才推升計畫",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const golden = [
-	{
-		name: "西坑銀行",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑有點軟",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑銀行",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑有點軟",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑銀行",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑有點軟",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const silver = [
-	{
-		name: "sitcondo",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "sitconsky",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const bronze = [
-	{
-		name: "五倍吸坑",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑科技",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "西坑基金會",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const thanks = [
-	{
-		name: "西坑青年發展署促進青年公共參與補助",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "中國西坑大學",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
 
 export const medias = [
-	{
-		name: "西坑傳媒",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
-	{
-		name: "SitconNews",
-		logo: "sitcon.png",
-		intro: sitconIntro,
-	},
 ]
