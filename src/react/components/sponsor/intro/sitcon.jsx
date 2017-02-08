5848741fcc1bc6caacb5bf9470e14b3e3a7b0517
Sunset <s2821d3721@gmail.com>
2017-02-08 17:18:43 +0800

    [sponsor] Update sitcon intro

--- src/react/components/sponsor/intro/sitcon.jsx
+++ src/react/components/sponsor/intro/sitcon.jsx
@@ -6,12 +6,12 @@ export default function(props) {
 			<p>{"SITCON（Students' Information Technology Conference, 學生計算機年會）是自 2013 年由一群學生自主性地發起，是一個以在學學生為主體的學生社群。以自發性的學習為基礎，秉持開源、創新與實作的核心理念。"}</p>
 			<p>{"我們希望藉由每年三月固定的研討會（SITCON）、每學期的 hackathon （駭客世代 HackGen）、每年暑假的夏令營及兩個禮拜定期聚會的方式，將全台灣各地對於資訊科技相關領域有興趣的學生們串聯起來，透過彼此共享、交流來切磋精進，砥礪自主自學的風氣。"}</p>
 			<ul>
-				<li><a>臉書交流區</a></li>
-				<li><a>Hackgen</a></li>
-				<li><a>Camp</a></li>
-				<li><a>定期聚</a></li>
-				<li><a>Youtube（有歷年影片！）</a></li>
-				<li><a>sitcon-general@googlegroups.com</a></li>
+				<li><a target="blank" href="https://www.facebook.com/groups/sitcon.tw">臉書交流區</a></li>
+				<li><a target="blank" href="http://hackgen.sitcon.org/">Hackgen</a></li>
+				<li><a target="blank" href="http://sitcon.camp/">Camp</a></li>
+				<li><a target="blank" href="http://sitcon.kktix.cc/">定期聚</a></li>
+				<li><a target="blank" href="https://www.youtube.com/user/SITCONArchive">Youtube（有歷年影片！）</a></li>
+				<li>sitcon-general@googlegroups.com</li>
 			</ul>
 		</div>
 	)
