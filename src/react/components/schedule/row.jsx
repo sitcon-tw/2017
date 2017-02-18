7963eb62d61e455e401da9b8ccfd94f5bf6d21b9
Denny Huang <denny0223@gmail.com>
2017-02-19 00:35:36 +0800

    Revert "[schedule] Hard code special rowspan"
    This reverts commit 2f21a5b31af4317ac2b8804c797d081b4fdb0354.

--- src/react/components/schedule/row.jsx
+++ src/react/components/schedule/row.jsx
@@ -9,25 +9,12 @@ export default function(props) {
 	const contentR1 = makeContent(props.data["R1"])
 	const contentR2 = makeContent(props.data["R2"])
 	const contentR3 = makeContent(props.data["R3"])
-
-	let R2 = null
-	if( props.data["R2"] && props.data["R2"].subject === "校園資訊系統的困境與嘗試改善" )
-		R2 = (
-			<td onClick={() => { fancybox(contentR2) }} className="clickable" rowSpan={3}>
-				<Col data={props.data["R2"]} />
-			</td>
-		)
-	else if( props.data["R2"] )
-		R2 = (
-			<td onClick={() => { fancybox(contentR2) }} className="clickable">
-				<Col data={props.data["R2"]} />
-			</td>
-		)
-
 	return (
 		<tr>
 			<Timecol start={props.start} />
-			{R2}
+			<td onClick={() => { fancybox(contentR2) }} className="clickable">
+				<Col data={props.data["R2"]} />
+			</td>
 			<td onClick={() => { fancybox(contentR0) }} className="clickable">
 				<Col data={props.data["R0"]} />
 			</td>
