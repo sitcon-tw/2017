68d03b8d215f7c66ee08740642085fd1b2d850f9
Denny Huang <denny0223@gmail.com>
2017-02-20 19:08:02 +0800

    Add sponsors

---
+++ src/react/components/sponsor/intro/hackmd.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default function(props) {
+	return (
+		<div>
+			<p>{"HackMD 提供您建立即時的跨平台 Markdown 筆記。"}</p>
+      <p>{"能用於組織您的想法、與夥伴討論並即時預覽結果，不但如此，同時也能支援簡報、圖表、數學表達式。"}</p>
+      <p>{" 從上線以來，共超過 9 萬份筆記被寫在 HackMD 上，我們幫助使用者製作文件、便條、工作事項、write-ups、作業、教學甚至提供開放討論的平台。"}</p>
+      <p>{" 現在就加入吧！您也能用聰明的方式寫作！"}</p>
+      <p>{"一用就愛上 <3"}</p>
+		</div>
+	)
+}
--- src/react/components/sponsor/sponsor.jsx
+++ src/react/components/sponsor/sponsor.jsx
@@ -1,4 +1,10 @@
 import sitconIntro from './intro/sitcon.jsx'
+import iisIntro from './intro/iis.jsx'
+import ocfIntro from './intro/ocf.jsx'
+import itsaIntro from './intro/itsa.jsx'
+import archlifeIntro from './intro/archlife.jsx'
+import hackmdIntro from './intro/hackmd.jsx'
+import cnexIntro from './intro/cnex.jsx'
 
 export const organizer = [
 	{
@@ -6,21 +12,51 @@ export const organizer = [
 		logo: "sitcon.png",
 		intro: sitconIntro,
 	},
+	{
+		name: "中央研究院資訊科學研究所",
+		logo: "iis.png",
+		intro: iisIntro,
+	},
+	{
+		name: "財團法人開放文化基金會",
+		logo: "ocf.png",
+		intro: ocfIntro,
+	},
 ]
 
 export const coorganizer = [
+	{
+		name: "教育部資通訊軟體創新人才推升計畫推動中心",
+		logo: "itsa.png",
+		intro: itsaIntro,
+	},
 ]
 
 export const golden = [
 ]
 
 export const silver = [
+	{
+		name: "祐生基金會",
+		logo: "archlife.png",
+		intro: archlifeIntro,
+	},
 ]
 
 export const bronze = [
 ]
 
 export const thanks = [
+	{
+		name: "HackMD",
+		logo: "hackmd.png",
+		intro: hackmdIntro,
+	},
+	{
+		name: "CNEX 基金會",
+		logo: "cnex.png",
+		intro: cnexIntro,
+	},
 ]
 
 export const medias = [
